4f529be6747f3f700cbe740ebed7c40dae1dc07c
Stanislav Bezsonov <neosporimiy999@gmail.com>
2025-07-17 17:34:05 +0200

    FireBase fix + UI fix

---
+++ BudgetApp/CommonUI/DashboardViewController/CenterAlignedCollectionViewFlowLayout.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+class LeftAlignedCollectionViewFlowLayout: UICollectionViewFlowLayout {
+    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        guard let attributes = super.layoutAttributesForElements(in: rect) else { return nil }
+
+        var leftMargin = sectionInset.left
+        var maxY: CGFloat = -1.0
+
+        attributes.forEach { layoutAttribute in
+            if layoutAttribute.representedElementCategory == .cell {
+                if layoutAttribute.frame.origin.y >= maxY {
+                    leftMargin = sectionInset.left
+                }
+                layoutAttribute.frame.origin.x = leftMargin
+                leftMargin += layoutAttribute.frame.width + minimumInteritemSpacing
+                maxY = max(layoutAttribute.frame.maxY, maxY)
+            }
+        }
+        return attributes
+    }
+
+    override func shouldInvalidateLayout(forBoundsChange newBounds: CGRect) -> Bool {
+        return true
+    }
+}
